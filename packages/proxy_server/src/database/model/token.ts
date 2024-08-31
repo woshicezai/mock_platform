eaa62e9ed535c31ee27e97028a20f372f789a8ab
woshicezai <behappys@sina.com>
2024-08-31 22:00:52 +0800

    feat: 上个提交遗漏了token文件

---
+++ packages/proxy_server/src/database/model/token.ts
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose'
+
+const tokenSchema = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  token: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    expires: 3600 // 1小时后过期
+  },
+  isRevoked: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const Token = model('Token', tokenSchema);
+
+module.exports = Token;
